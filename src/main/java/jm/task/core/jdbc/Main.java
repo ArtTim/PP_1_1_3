898bdd350893e8d3b94226d35e1484f88e6f3c99
ArtTim <tim-murr@inbox.ru>
2022-07-16 18:01:26 +0300

    Added all methods.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,28 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Vlad", "Ok", (byte)15);
+        userService.saveUser("Sergey", "Buza", (byte)27);
+        userService.saveUser("Igor", "Dolinnik", (byte)25);
+        userService.getAllUsers();
+        for(User el : userService.getAllUsers()) {
+            System.out.println(el);
+        }
+        userService.removeUserById(3);
+        System.out.println("После удаления:");
+        userService.getAllUsers();
+        for(User el : userService.getAllUsers()) {
+            System.out.println(el);
+        }
+        userService.dropUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,90 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl extends Util implements UserService {
+
+
     public void createUsersTable() {
 
+        String sql = "CREATE TABLE IF NOT EXISTS `users` (id BIGINT NOT NULL AUTO_INCREMENT, name VARCHAR(45) NOT NULL, lastName VARCHAR(45) NULL, age INT NULL, PRIMARY KEY (id))";
+
+        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
 
+        String sql = "DROP TABLE IF EXISTS `users`";
+
+        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        String sql = "insert into `users` (name, lastName, age) values (?, ?, ?)";
 
+        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        String sql = "DELETE FROM `users` WHERE id= ?";
+        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+            preparedStatement.setString(1, String.valueOf(id));
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> userList = new ArrayList<>();
+        String sql = "select name, lastName, age from `users`";
+
+        try (Connection connection = getConnection(); Statement statement = connection.prepareStatement(sql)){
+
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while(resultSet.next()) {
+                User user = new User(resultSet.getString("name"), resultSet.getString("lastName"), resultSet.getByte("age"));
+                userList.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return userList;
     }
 
     public void cleanUsersTable() {
+        String sql = "DELETE FROM `users`";
+
+        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 }
